876fa317182a4eb6cbf68195ba5f77c446eca425
Indra Prasetya <indraprasetya154@gmail.com>
2021-03-30 10:25:10 +0700

    Refactor maxShift code

--- app/Http/Requests/CommandCenterReservationCreateRequest.php
+++ app/Http/Requests/CommandCenterReservationCreateRequest.php
@@ -28,7 +28,7 @@ class CommandCenterReservationCreateRequest extends FormRequest
     {
         $oneMonthLater =  Carbon::now()->addMonth();
         $shift = CommandCenterShift::find($this->input('command_center_shift_id'));
-        $maxShift = ($shift) ? $shift->capacity : config('shift.default_max_visitor');
+        $maxShift = optional($shift)->capacity ?? config('shift.default_max_visitor');
         return [
             'name' => 'string|required|max:100',
             'nik' => 'required|max:16|regex:/[0-9]{16}/',
@@ -37,7 +37,7 @@ class CommandCenterReservationCreateRequest extends FormRequest
             'phone_number' => 'required|min:10|max:13|regex:/(0)[0-9]/',
             'email' => 'required|email:rfc,dns',
             'purpose' => 'string|required|max:255',
-            'reservation_date' => ['required|date|date_format:Y-m-d|after_or_equal:today|before_or_equal:' . $oneMonthLater],
+            'reservation_date' => 'required|date|date_format:Y-m-d|after_or_equal:today|before_or_equal:' . $oneMonthLater,
             'command_center_shift_id' => 'required|exists:command_center_shifts,id,deleted_at,NULL',
             'visitors' => [
                 'integer', 'required', 'min:1', 'max:' . $maxShift,
