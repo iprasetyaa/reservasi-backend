f92da99f91b051dec86d723f84083b8a8ff58c91
samudra-ajri <samudra_ajri@live.com>
2021-04-05 13:44:47 +0700

    clean code

--- app/Http/Requests/CommandCenterReservationCreateRequest.php
+++ app/Http/Requests/CommandCenterReservationCreateRequest.php
@@ -27,7 +27,6 @@ class CommandCenterReservationCreateRequest extends FormRequest
      */
     public function rules()
     {
-        $oneMonthLater =  Carbon::now()->addMonth();
         $shift = CommandCenterShift::find($this->input('command_center_shift_id'));
         $maxShift = optional($shift)->capacity ?? config('shift.default_max_visitor');
         return [
@@ -38,16 +37,11 @@ class CommandCenterReservationCreateRequest extends FormRequest
             'phone_number' => 'required|min:10|max:13|regex:/(0)[0-9]/',
             'email' => 'required|email:rfc,dns',
             'purpose' => 'string|required|max:255',
-            'reservation_date' => 'required|date|date_format:Y-m-d|after_or_equal:today|before_or_equal:' . $oneMonthLater,
+            'reservation_date' => 'required|date|date_format:Y-m-d|after_or_equal:today|before_or_equal:' . Carbon::now()->addMonth(),
             'command_center_shift_id' => 'required|exists:command_center_shifts,id,deleted_at,NULL',
             'visitors' => [
                 'integer', 'required', 'min:1', 'max:' . $maxShift,
-                new CommandCenterReservationRule(
-                    $this->command_center_shift_id,
-                    $this->reservation_date,
-                    $this->visitors,
-                    $maxShift
-                )
+                new CommandCenterReservationRule($this->command_center_shift_id, $this->reservation_date, $this->visitors, $maxShift)
             ],
             'recaptcha' => [
                 new GoogelRecaptchaRule($this->recaptcha)
