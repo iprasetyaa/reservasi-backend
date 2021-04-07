898d4dd9dff3176094be348de6cf3741e10e5ecc
samudra-ajri <samudra_ajri@live.com>
2021-04-07 18:37:48 +0700

    add filter a month functionality

--- app/Http/Controllers/V1/CommandCenterCloseDaysListController.php
+++ app/Http/Controllers/V1/CommandCenterCloseDaysListController.php
@@ -5,22 +5,39 @@ namespace App\Http\Controllers\V1;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\CCCloseDateListResource;
 use App\Models\CommandCenterCloseDate;
-use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class CommandCenterCloseDaysListController extends Controller
 {
     /**
-     * Handle the incoming request.
+     * Send disabled days, dates only.
      *
-     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
     public function __invoke()
     {
         $records = CommandCenterCloseDate::query();
-        $records = $records->orderBy('date')->get();
+        $records = $records->orderBy('date');
 
-        return CCCloseDateListResource::collection($records);
+        $records = $this->filterAMonth($records);
+
+        return CCCloseDateListResource::collection($records->get());
+    }
+
+    /**
+     * filter a month ahead
+     *
+     * @param  [Collection] $records
+     * @return Collection
+     */
+    protected function filterAMonth($records)
+    {
+        $from = Carbon::now()->toDateString();
+        $to = Carbon::now()->addMonth();
+
+        $records->whereBetween('date', [$from, $to]);
+
+        return $records;
     }
 
 }
