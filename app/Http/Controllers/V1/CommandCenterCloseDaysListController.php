80fb8500a7e52c8f0de401cb2c7cce5010b37962
samudra-ajri <samudra_ajri@live.com>
2021-04-08 15:05:35 +0700

    add local scope method

--- app/Http/Controllers/V1/CommandCenterCloseDaysListController.php
+++ app/Http/Controllers/V1/CommandCenterCloseDaysListController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\V1;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\CCCloseDateListResource;
 use App\Models\CommandCenterCloseDate;
-use Carbon\Carbon;
 
 class CommandCenterCloseDaysListController extends Controller
 {
@@ -16,27 +15,8 @@ class CommandCenterCloseDaysListController extends Controller
      */
     public function __invoke()
     {
-        $records = CommandCenterCloseDate::query();
-        $records = $records->orderBy('date');
-
-        $records = $this->filterAMonth($records);
+        $records = CommandCenterCloseDate::filterAMonth()->orderBy('date');
 
         return CCCloseDateListResource::collection($records->get());
-    }
-
-    /**
-     * filter a month ahead
-     *
-     * @param  [Collection] $records
-     * @return Collection
-     */
-    protected function filterAMonth($records)
-    {
-        $from = Carbon::now()->toDateString();
-        $to = Carbon::now()->addMonth();
-
-        $records->whereBetween('date', [$from, $to]);
-
-        return $records;
     }
 }
--- app/Models/CommandCenterCloseDate.php
+++ app/Models/CommandCenterCloseDate.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class CommandCenterCloseDate extends Model
@@ -14,4 +15,18 @@ class CommandCenterCloseDate extends Model
     protected $casts = [
         'date' => 'datetime:Y-m-d'
     ];
+
+    /**
+     * Scope a query to only include dates within a month from now.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopefilterAMonth($query)
+    {
+        $from = Carbon::now()->toDateString();
+        $to = Carbon::now()->addMonth();
+
+        return $query->whereBetween('date', [$from, $to]);
+    }
 }
