59f69ba251af68077a3b17dace6417770dfd249c
samudra-ajri <samudra_ajri@live.com>
2021-04-12 10:43:45 +0700

    modify mail notification date format

--- resources/views/emails/reservationStore.blade.php
+++ resources/views/emails/reservationStore.blade.php
@@ -5,8 +5,8 @@ Melalui surat elektronik ini, kami memberitahukan bahwa ada data reservasi yang 
 - Nama Pegawai: {{ $reservation->user_fullname }}
 - Judul Kegiatan: {{ $reservation->title }}
 - Catatan Kegiatan: {{ $reservation->description }}
-- Tanggal dan Waktu Kegiatan: {{ $reservation->start_time }} sd. {{ $reservation->end_time }}
-- Tanggal Dibuat: {{ $reservation->created_at }}
+- Tanggal dan Waktu Kegiatan: {{ date('d-m-Y H:i', strtotime($reservation->start_time)) }} sd. {{ date('d-m-Y H:i', strtotime($reservation->end_time)) }}
+- Tanggal Dibuat: {{ date('d-m-Y', strtotime($reservation->created_at)) }}
 
 @component('mail::button', ['url' => $url])
 Lihat Reservasi
