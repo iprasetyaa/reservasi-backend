0012908de7d4f605ae1e79e6b02dae96a31eabb3
samudra-ajri <samudra_ajri@live.com>
2021-05-03 08:03:19 +0700

    add new messages

--- resources/lang/en/message.php
+++ resources/lang/en/message.php
@@ -7,4 +7,5 @@ return [
     'already_approved' => 'Approved',
     'not_yet_approved' => 'On Progress',
     'no_reservation' => 'No reservation occurrences',
+    'already_reserved' => 'Asset has been reserved at the time chosen',
 ];
