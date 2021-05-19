76829c4650080b9ef9a68c9a9f743dc9ab85be48
Indra Prasetya <indraprasetya154@gmail.com>
2021-05-19 11:49:16 +0700

    Mail template on empty description non repeat

--- resources/views/emails/reservationApproval.blade.php
+++ resources/views/emails/reservationApproval.blade.php
@@ -5,7 +5,7 @@ Terima kasih Anda sudah melakukan reservasi pada Aplikasi Digiteam Reservasi Ase
 Melalui surat elektronik ini, berdasarkan data reservasi yang kami terima yaitu:
 - Nama : {{ $data[0]['reservation']->user_fullname }}
 - Judul Kegiatan: {{ $data[0]['reservation']->title }}
-- Catatan Kegiatan: {{ $data[0]['reservation']->description }}
+- Catatan Kegiatan: {{ $data[0]['reservation']->description ?? '-' }}
 - Tanggal dan Waktu Kegiatan: {{ $data[0]['reservation']->start_time->format('d-m-Y H:i') }} sd. {{ $data[0]['reservation']->end_time->format('d-m-Y H:i') }}
 - Tanggal Dibuat: {{ $data[0]['reservation']->created_at->format('d-m-Y') }}
 
